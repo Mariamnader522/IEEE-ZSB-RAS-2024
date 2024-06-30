64be009d2a353749c61fdab6c31e1eeb227b85c6
mariam Nader <mariamnader522@gmail.com>
2024-07-01 00:02:28 +0300

    task10

---
+++ task10/first_project/seven segment.h
@@ -0,0 +1,10 @@
+/*
+ * seven_segment.h
+ *
+ */
+
+#ifndef SEVEN_SEGMENT_H_
+#define SEVEN_SEGMENT_H_
+void seven_seg_start(unsigned char port_name);
+void seven_seg_write(unsigned char portname,unsigned char number);
+#endif
